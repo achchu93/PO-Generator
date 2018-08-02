6ae400f751cb109db4c50a7309f2293430d2b2f1
achchu93 <achchu.zats@gmail.com>
2018-08-03 01:30:42 +0530

    added: .po file enabler added: gravity form hook

--- includes/wptg_functions.php
+++ includes/wptg_functions.php
@@ -1,97 +1,72 @@
 <?php
 
-/*
-* Admin menu
-*/
-add_action('admin_menu', 'wptg_menu_pages');
-
-function wptg_menu_pages(){
-    
-    add_menu_page('WP Translation Generator', 'Translation Generator', 'manage_options', 'translation-generator', 'wptg_settings_output', 'dashicons-editor-customchar' );
-
-}
-
-function wptg_settings_output(){
-
-	$selected = get_option('wptg_gravity_forms', array());
-	$g_forms = GFAPI::get_forms();
-	?>
-
-	<div id="wpbody-content">
-
-		<div class="wrap">
-			
-			<h1><?php _e('WP Translation Generator', 'wptg'); ?></h1>
-
-			<form method="post" action="<?php echo admin_url('admin-post.php'); ?>">
-
-				<p class="form-row">
-
-					<label for="wptg_gravity_forms"><?php _e('Gravity Forms', 'wptg'); ?></label>
-					<select name="wptg_gravity_forms[]" id="wptg_gravity_forms" multiple>
-                        <?php
-                            if( count($g_forms) ){
-
-                                foreach ( $g_forms as $g_form ) :
-                                    ?>
-
-                                    <option value="<?php echo $g_form['id'] ?>" <?php echo  in_array($g_form['id'], $selected) ? "selected" : "" ?> ><?php echo $g_form['title'] ?></option>
-
-                                <?php endforeach;
-
-                            }
-                            else{
-                            ?>
-                                <option value="0" disabled ><?php _e('No Gravity Forms Found', 'wptg')?></option>
-                            <?php
-                            }
-                        ?>
-                    </select>
-
-				</>
-
-				<?php
-
-					wp_nonce_field('wptg_setting_save', 'wptg_settings_nonce');
-
-					submit_button();
-
-				?>
-
-			</form>
-		
-		</div>		
-	
-	</div>
-
-
-	<?php
-
-}
-
-
-/*
-* Saving setting page
-*/
-
+add_action('admin_menu', 'wptg_menu_page');
 add_action('admin_post', 'wptg_setting_save');
+add_action( 'admin_enqueue_scripts', 'wptg_enqueue_scripts' );
+add_filter('upload_mimes', 'add_po_mime_type', 1, 1);
+add_action( 'gform_after_submission', 'generate_translation', 10, 2 );
 
-function wptg_setting_save(){
 
-	if( !isset($_POST['wptg_settings_nonce']) || !wp_verify_nonce($_POST['wptg_settings_nonce'], 'wptg_setting_save') ){
-
-		redirect_to_wptg_settings();
-
-	}
-
-	if( isset($_POST['wptg_gravity_forms']) && !empty($_POST['wptg_gravity_forms']) )
-
-		update_option('wptg_gravity_forms', $_POST['wptg_gravity_forms']);
-
-	redirect_to_wptg_settings();
-
+/**
+ * add admin menu page
+ */
+function wptg_menu_pages(){
+    add_menu_page('WP Translation Generator', 'Translation Generator', 'manage_options', 'translation-generator', 'wptg_settings_output', 'dashicons-editor-customchar' );
 }
 
+/**
+ * admin menu page output
+ */
+function wptg_settings_output(){
+    $selected = get_option('wptg_gravity_forms', array());
+    $g_forms = GFAPI::get_forms();
+?>
+
+    <div id="wpbody-content">
+        <div class="wrap">
+            <h1><?php _e('WP Translation Generator', 'wptg'); ?></h1>
+
+            <form method="post" action="<?php echo admin_url('admin-post.php'); ?>">
+                <p class="form-row">
+                    <label for="wptg_gravity_forms"><?php _e('Gravity Forms', 'wptg'); ?></label>
+                    <select name="wptg_gravity_forms[]" id="wptg_gravity_forms" multiple>
+                        <?php
+                        if(count($g_forms)) :
+                            foreach ($g_forms as $g_form) : ?>
+                                <option value="<?php echo $g_form['id'] ?>" <?php echo  in_array($g_form['id'], $selected) ? "selected" : "" ?> ><?php echo $g_form['title'] ?></option>
+                            <?php endforeach; ?>
+                        <?php else : ?>
+                            <option value="0" disabled ><?php _e('No Gravity Forms Found', 'wptg')?></option>
+                        <?php endif; ?>
+                    </select>
+                </p>
+            </form>
+        </div>
+    </div>
+    <?php
+}
+
+/**
+ * save admin settings
+ */
+function wptg_setting_save()
+{
+    if (!isset($_POST['wptg_settings_nonce']) || !wp_verify_nonce($_POST['wptg_settings_nonce'], 'wptg_setting_save')) {
+        redirect_to_wptg_settings();
+    }
+
+    if (isset($_POST['wptg_gravity_forms'])){
+        update_option('wptg_gravity_forms', $_POST['wptg_gravity_forms']);
+    }else{
+        delete_option('wptg_gravity_forms');
+    }
+
+    redirect_to_wptg_settings();
+}
+
+/**
+ * redirect to plugin admin page
+ */
 function redirect_to_wptg_settings(){
 
 	$url = !isset($_POST['_wp_http_referer']) ? admin_url('page=translation-generator') : $_POST['_wp_http_referer'];
@@ -99,6 +74,9 @@ function redirect_to_wptg_settings(){
 	wp_safe_redirect($url);
 }
 
+/**
+ * enqueueing admin styles and scripts
+ */
 function wptg_enqueue_scripts() {
 
     global $current_screen;
@@ -110,5 +88,27 @@ function wptg_enqueue_scripts() {
     wp_enqueue_script( 'wptg-select2-js', WPTG_URL . 'assets/js/select2.full.min.js' , array('jquery'), null, true );
     wp_enqueue_script( 'wptg-admin-js', WPTG_URL . 'assets/js/admin.js' , array('jquery'), null, true );
 }
-add_action( 'admin_enqueue_scripts', 'wptg_enqueue_scripts' );
+
+/**
+ * enable .po file type uploading
+ *
+ * @param $mime_types
+ * @return mixed
+ */
+function add_po_mime_type($mime_types){
+    $mime_types['po'] = 'text/x-gettext-translation'; //Adding svg extension
+    return $mime_types;
+}
+
+/**
+ * do generating after form submission
+ * @param $entry
+ * @param $form
+ */
+function generate_translation($entry, $form ) {
+
+    GFCommon::log_debug( 'gform_after_submission: body => ' . print_r( $form, true ) );
+    GFCommon::log_debug( 'gform_after_submission: response => ' . print_r( $entry, true ) );
+}
+
 
